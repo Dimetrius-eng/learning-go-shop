4463fb45083e52c8e8d646b0c1c8b7bc7671a98b
Dimetrius-eng <mydimka08@gmail.com>
2026-05-15 12:31:00 +0300

    fix docker version mistake

--- internal/services/upload_service.go
+++ internal/services/upload_service.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"fmt"
+	"github.com/google/uuid"
 	"mime/multipart"
 	"path/filepath"
 	"strings"
@@ -24,7 +25,9 @@ func (s *UploadService) UploadProductImage(productID uint, file *multipart.FileH
 		return "", fmt.Errorf("invalid file type: %s", ext)
 	}
 
-	path := fmt.Sprintf("products/%d/%s", productID, file.Filename)
+	newFileName := uuid.New().String()
+
+	path := fmt.Sprintf("products/%d/%s%s", productID, newFileName, ext)
 
 	return s.provider.UploadFile(file, path)
 }
