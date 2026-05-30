e81d47691e03117fc16caa1630b106546c8a22e1
Dimetrius-eng <mydimka08@gmail.com>
2026-05-30 08:54:25 +0300

    add implementation for order resolvers

--- graph/resolver/helpers.go
+++ graph/resolver/helpers.go
@@ -48,25 +48,3 @@ func IsAdminFromContext(ctx context.Context) bool {
 
 	return role == adminRole
 }
-
-func getPagingNumbers(page, limit *int) (pageNumber, pageLimit int) {
-	var p, l = 0, 0
-
-	if page != nil {
-		p = *page
-	}
-
-	if limit != nil {
-		l = *limit
-	}
-
-	if p <= 0 {
-		p = 1
-	}
-
-	if l <= 0 {
-		l = 10
-	}
-
-	return p, l
-}
